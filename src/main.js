13e45bf363ca25fb8e810b734f643b8085379688
Daxi Gudjónsson <dg@learningbank.io>
2022-01-08 15:29:35 +0100

    Installed fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 
-createApp(App).mount("#app");
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
