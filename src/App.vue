0110eb4452ec951f812a632770afb5bfff389798
Daxi Gudjónsson <dg@learningbank.io>
2022-01-08 13:11:36 +0100

    Inital commit - new vite project - installed and configured tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    data: () => ({
+      value: 'Hello world!'
+    })
+  }
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    {{ value }}
+  </h1>
+</template>
